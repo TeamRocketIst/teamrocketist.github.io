bca463af9ff164923573d3fdfbf72dba723830c1
lolstorm92 <lolstorm92@Debian.home>
2017-08-10 17:51:14 +0100

    Site updated: 2017-08-10 17:51:14

--- js/insight.js
+++ js/insight.js
@@ -30,7 +30,7 @@
         sectionTitle = CONFIG.TRANSLATION[type];
         switch (type) {
             case 'POSTS':
-            case 'PAGES':
+            //case 'PAGES':
                 $searchItems = array.map(function (item) {
                     // Use config.root instead of permalink to fix url issue
                     return searchItem('file', item.title, null, item.text.slice(0, 150), CONFIG.ROOT_URL + item.path);
